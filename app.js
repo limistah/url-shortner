085e2a65672bf90ae4bce3aa7f3b71b84261af9a
Aleem Isiaka <limistah@AleemPro.local>
2020-09-28 18:27:54 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const loki = require("lokijs");
+const randomstring = require("randomstring");
+
+var db = new loki("urlShortener");
+var shortLinkDB = db.addCollection("shortLinkDB", {
+  indices: ["code", "visitCount"],
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors({}));
+
+const PORT = process.env.PORT || 8899;
+
+app.get("/", (req, res, next) => {
+  return res.json({ status: "Online" });
+});
+
+app.post("/shorten", function (req, res, next) {
+  let { url, code } = req.body;
+  code = code || randomstring.generate(5);
+
+  const linkExists = shortLinkDB.findOne({ code });
+
+  if (linkExists) {
+    return res.json({ error: true, message: "Code already in use" });
+  }
+
+  const shortLink = shortLinkDB.insert({
+    code,
+    url,
+  });
+  res.json(shortLink);
+});
+
+app.get("/:code", function (req, res, next) {
+  let { code } = req.params;
+
+  const shortenedURL = shortLinkDB.findOne({ code });
+
+  if (!shortenedURL) {
+    return res.json({ error: true, message: `No URL matching code ${code}` });
+  }
+
+  (shortenedURL.visitCount = (shortenedURL.visitCount || 0) + 1),
+    shortLinkDB.update(shortenedURL);
+
+  res.redirect(shortenedURL.url);
+});
+
+app.listen(PORT, () => {
+  console.log(`Application is listening on ${PORT}`);
+});
